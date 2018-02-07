c0852a3ebb5534fb84f682c120239a766631c97c
Nazarii_Moshenskyi <nazarii_moshenskyi@epam.com>
2018-02-07 15:35:35 +0200

    Removed unused files.

--- MultithreadingTask/app/src/main/java/com/example/nazarii_moshenskyi/multithreadingtask/presentation/base/BaseMvpPresenter.java
+++
@@ -1,9 +0,0 @@
-package com.example.nazarii_moshenskyi.multithreadingtask.presentation.base;
-
-public interface BaseMvpPresenter<T extends BaseMvpView> {
-
-    void attachView(T view);
-
-    void detachView();
-
-}
--- MultithreadingTask/app/src/main/java/com/example/nazarii_moshenskyi/multithreadingtask/presentation/base/BaseMvpView.java
+++
@@ -1,4 +0,0 @@
-package com.example.nazarii_moshenskyi.multithreadingtask.presentation.base;
-
-public interface BaseMvpView {
-}
